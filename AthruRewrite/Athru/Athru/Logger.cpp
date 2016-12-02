867cd7965448fa927028eb9aca1c5b662fca1bb0
Paul Ferris <peferris@internode.on.net>
2016-12-02 16:39:02 +1100

    Began implementing templated logger class

--- AthruRewrite/Athru/Athru/Logger.cpp
+++ AthruRewrite/Athru/Athru/Logger.cpp
@@ -1,9 +1,116 @@
+#include "stdlib.h"
 #include "Logger.h"
 
-Logger::Logger()
+Logger::Logger(char* loggingTo)
 {
+	logFilePath = loggingTo;
 }
 
 Logger::~Logger()
 {
+}
+
+void Logger::LogTextCharToCMD(char textCharLogging)
+{
+	printf("%s", stringLogging);
+}
+
+void Logger::LogSignedByteToCMD(byteSigned signedByteLogging)
+{
+	printf("%s", stringLogging);
+}
+
+void Logger::LogUnsignedByteToCMD(byteUnsigned unsignedByteLogging)
+{
+	printf("%s", stringLogging);
+}
+
+void Logger::LogSignedDoubleByteToCMD(twoByteSigned signedDoubleByteLogging)
+{
+	printf("%s", stringLogging);
+}
+
+void Logger::LogUnsignedDoubleByteToCMD(twoByteUnsigned UnsignedDoubleByteLogging)
+{
+	printf("%s", stringLogging);
+}
+
+void Logger::LogSignedQuadByteToCMD(fourByteSigned unsignedByteLogging)
+{
+	printf("%s", stringLogging);
+}
+
+void Logger::LogUnsignedQuadByteToCMD(fourByteUnsigned unsignedByteLogging)
+{
+	printf("%s", stringLogging);
+}
+
+void Logger::LogSignedOctaByteToCMD(eightByteSigned unsignedByteLogging)
+{
+	printf("%s", stringLogging);
+}
+
+void Logger::LogUnsignedOctaByteToCMD(eightByteUnsigned unsignedOctaByteLogging)
+{
+	printf("%ull", unsignedByteLogging);
+}
+
+void Logger::LogAddressToCMD(void* addressLogging)
+{
+	printf("%p", addressLogging);
+}
+
+void Logger::LogStringToFile(char* stringLogging)
+{
+
+}
+
+void Logger::LogTextCharToFile(char textCharLogging)
+{
+
+}
+
+void Logger::LogSignedByteToFile(byteSigned signedByteLogging)
+{
+
+}
+
+void Logger::LogUnsignedByteToFile(byteUnsigned unsignedByteLogging)
+{
+
+}
+
+void Logger::LogSignedDoubleByteToFile(twoByteSigned signedDoubleByteLogging)
+{
+
+}
+
+void Logger::LogUnsignedDoubleByteToFile(twoByteUnsigned UnsignedDoubleByteLogging)
+{
+
+}
+
+void Logger::LogSignedQuadByteToFile(fourByteSigned unsignedByteLogging)
+{
+
+}
+
+void Logger::LogUnsignedQuadByteToFile(fourByteUnsigned unsignedByteLogging)
+{
+
+}
+
+void Logger::LogSignedOctaByteToFile(eightByteSigned unsignedByteLogging)
+{
+
+}
+
+void Logger::LogUnsignedOctaByteToFile(eightByteUnsigned unsignedByteLogging)
+{
+
+}
+
+void Logger::LogStringToCMD(char* stringLogging)
+{
+	printf("%s", stringLogging);
 }
--- AthruRewrite/Athru/Athru/Logger.h
+++ AthruRewrite/Athru/Athru/Logger.h
@@ -1,9 +1,61 @@
+#include <iostream>
+#include "Typedefs.h"
 #pragma once
 
-class Logger
+template<typename loggableType> class Logger
 {
 	public:
-		Logger();
+		Logger(char* loggingTo);
 		~Logger();
+
+		// Functions logging data to CMD (single variables)
+		void LogTextCharToCMD(char textCharLogging);
+		void LogSignedByteToCMD(byteSigned signedByteLogging);
+		void LogUnsignedByteToCMD(byteUnsigned unsignedByteLogging);
+		void LogSignedDoubleByteToCMD(twoByteSigned signedDoubleByteLogging);
+		void LogUnsignedDoubleByteToCMD(twoByteUnsigned UnsignedDoubleByteLogging);
+		void LogSignedQuadByteToCMD(fourByteSigned unsignedByteLogging);
+		void LogUnsignedQuadByteToCMD(fourByteUnsigned unsignedByteLogging);
+		void LogSignedOctaByteToCMD(eightByteSigned unsignedByteLogging);
+		void LogUnsignedOctaByteToCMD(eightByteUnsigned unsignedByteLogging);
+		void LogAddressToCMD(void* addressLogging);
+
+		// Logging data to CMD (single variables)
+		void LogToCMD(loggableType dataLogging)
+		{
+			// Check if type is printable before passing to std::cout
+		}
+
+		// Logging data to file (single variables)
+		void LogToFile(loggableType dataLogging)
+		{
+			
+		}
+
+		// Functions logging data to CMD (serial variables, e.g. arrays, strings, etc.)
+		void LogStringToCMD(char* stringLogging);
+		void LogSignedBytesToCMD(byteSigned* signedByteLogging);
+		void LogUnsignedBytesToCMD(byteUnsigned* unsignedByteLogging);
+		void LogSignedDoubleBytesToCMD(twoByteSigned* signedDoubleByteLogging);
+		void LogUnsignedDoubleBytesToCMD(twoByteUnsigned* UnsignedDoubleByteLogging);
+		void LogSignedQuadBytesToCMD(fourByteSigned* unsignedByteLogging);
+		void LogUnsignedQuadBytesToCMD(fourByteUnsigned* unsignedByteLogging);
+		void LogSignedOctaBytesToCMD(eightByteSigned* unsignedByteLogging);
+		void LogUnsignedOctaBytesToCMD(eightByteUnsigned* unsignedByteLogging);
+
+		// Functions logging data to file (serial variables, e.g. arrays and strings)
+		void LogStringToFile(char* stringLogging);
+		void LogSignedByteToFile(byteSigned signedByteLogging);
+		void LogUnsignedByteToFile(byteUnsigned unsignedByteLogging);
+		void LogSignedDoubleByteToFile(twoByteSigned signedDoubleByteLogging);
+		void LogUnsignedDoubleByteToFile(twoByteUnsigned UnsignedDoubleByteLogging);
+		void LogSignedQuadByteToFile(fourByteSigned unsignedByteLogging);
+		void LogUnsignedQuadByteToFile(fourByteUnsigned unsignedByteLogging);
+		void LogSignedOctaByteToFile(eightByteSigned unsignedByteLogging);
+		void LogUnsignedOctaByteToFile(eightByteUnsigned unsignedByteLogging);
+
+	private:
+		char* logFilePath;
+		// Memoize file data here
 };
 
